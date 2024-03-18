a0b49cfbb0ac48359314a8e28f523de4cddcb1bc
Tukimi1218 <159402460+Tukimi1218@users.noreply.github.com>
2024-03-18 15:44:51 +0900

    kadai_tutorial_outputをアップロード

---
+++ kadai_tutorial_output/test.js
@@ -0,0 +1,16 @@
+// ランダムなテキストを表示
+const createText = () => {};
+
+// キー入力の判定
+const keyPress = e => {
+    console.log(e.key);
+};
+
+// タイピングスキルのランクを判定
+const rankCheck = score => {};
+
+// ゲームを終了
+const gameOver = id => {};
+
+// カウントダウンタイマー
+const timer = () => {};
